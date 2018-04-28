ed5b6b1828a6b8e3d3db54409e92ceb0c7d7f345
akjeres <akjeres@gmail.com>
2018-04-28 20:36:44 +0300

    pre-structurizing completion

--- data/js/script.js
+++ data/js/script.js
@@ -1,1 +1,25 @@
-console.log(1);
+(function() {
+    var counter = 0;
+    function getScrollBarState() {
+        var result = {vScrollbar: true, hScrollbar: true};
+        try {
+            var root = document.compatMode=='BackCompat'? document.body : document.documentElement;
+            result.vScrollbar = root.scrollHeight > root.clientHeight;
+            result.hScrollbar = root.scrollWidth > root.clientWidth;
+        } catch(e) {}
+        return(result);
+    }
+
+    document.querySelector('.burger').onclick = function() {
+        counter++;
+        if (counter%2) {
+            document.querySelector('header').classList.add('active');
+        } else {
+            document.querySelector('header').classList.remove('active');
+        }
+    }
+})();
+
+/*if (getScrollBarState().vScrollbar) {
+    document.querySelector('html').classList.add('scroll');
+}*/
